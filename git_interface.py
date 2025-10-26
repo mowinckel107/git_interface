18aa61fcdb823991bfbe61ea0190ba2fe04649b4
Moose <mowinckel107@gmail.com>
2025-10-26 16:19:26 +0100

    Added more initiation configuration

--- git_interface.py
+++ git_interface.py
@@ -1,6 +1,5 @@
 
-
-
+from colored_text import *
 
 import sys
 import subprocess
@@ -10,14 +9,24 @@ from dataclasses import dataclass
 
 @dataclass
 class Git_Response:
-
-    # Attributes Declaration
-    # using Type Hints
     response_text: str
     return_code: int
 
 
-def run_git_command( command_line : str, directory_to_call_from : Optional[str] = None) -> Git_Response:
+QUIET : bool  = False; # This overrules VERBOSE
+VERBOSE : bool = True;
+
+REPO_PATH : str
+
+
+
+def set_repo_to_run_commands_on(repo_path : str):
+    global REPO_PATH
+    REPO_PATH = repo_path
+
+
+
+def run_git_command( command_line : str, directory_to_call_from : Optional[str] = None ) -> Git_Response:
     """Runs git command, returns output and status code."""
 
     command : list[str] = command_line.split()
@@ -33,4 +42,38 @@ def run_git_command( command_line : str, directory_to_call_from : Optional[str] 
     return_string += response_object.stderr.decode("utf-8")
 
     return Git_Response(return_string, response_object.returncode)
+
+
+
+def git_command(command : str, quiet : bool = False, allowed_to_fail : bool = False) -> Git_Response:
+
+    git_response : Git_Response = run_git_command(command, REPO_PATH)
+
+    if QUIET or quiet:
+        return git_response
+
+    print(f"command: ", end="")
+    print_text_green(command)
+
+    if VERBOSE:
+        print(f"return code: {git_response.return_code}")
+        print("git response:")
+        if git_response.return_code == 0:
+            print_text_yellow(git_response.response_text)
+        else:
+            print_text_red(git_response.response_text)
+    else:
+        print("")
+
+    print("")
+    print("")
+
+    if(not allowed_to_fail and git_response.return_code is not 0):
+        sys.exit(-1)
+
+    return git_response
+
+
+
+
 
